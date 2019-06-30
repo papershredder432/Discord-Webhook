754ea84d22ce75b5d32c0d955583f534ad604c10
jakub <48296454+flagsec@users.noreply.github.com>
2019-06-30 17:29:40 +0100

    Add files via upload

---
+++ Webhook Control/Webhook Control/Form1.cs
@@ -0,0 +1,44 @@
+﻿using Flagsec;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webhook_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Browse Text Files",
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Webhook hook = new Webhook("YOUR WEBHOOK URL HERE");
+            hook.Name = "YOUR NAME HERE";
+            hook.ProfilePictureUrl = "YOUR PICTURE URL HERE";
+
+            hook.SendMessage(textBox1.Text, textBox2.Text);
+        }
+    }
+}
